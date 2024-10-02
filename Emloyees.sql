64d8dbc44ea4196be77be183492073db7473bdec
Dmitriy Krasnyh <149979045+Iam3tonn@users.noreply.github.com>
2024-10-02 09:25:51 +0300

    Add files via upload

---
+++ Emloyees.sql
@@ -0,0 +1,3 @@
+﻿-- Script Date: понедельник.09.2024 3:58  - ErikEJ.SqlCeScripting version 3.5.2.95
+INSERT INTO [Employees] ([ID],[FullName],[Position],[Department],[Email]) VALUES (
+1,'Сергей Сидоров','Разработчик','Отдел разработки','sidorov@example.com');
